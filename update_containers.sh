3313dd0c79556e42ae5b701d7374f5624640c76a
Jbranco <james.branco@canadalife.com>
2021-09-18 18:56:27 -0400

    Added Automated. updates

--- update_containers.sh
+++ update_containers.sh
@@ -5,22 +5,26 @@ docker image prune -f
 
 # Kills all Containers
 docker kill $(docker ps -q)
-
-# Removes all containers
-docker rm $(docker ps -a -q)
-
-# Pulls Images
-docker pull linuxserver/plex
-docker pull alpine
-docker pull filebrowser/filebrowser
-docker pull nicolargo/glances
-docker pull nginx
-docker pull pihole/pihole 
-docker pull portainer/portainer-ce   
-docker pull mjenz/rpi-openvpn         
-
-# Runs Containers
-
+   
+# Pulls images and Runs Containers and updates them
+bash portainer.sh
+cd NGINX
+cd Proxy
+bash runner.sh
+cd..
+cd Webserver
+bash runner.sh
+cd ..
+cd Webserver2
+bash runner.sh
+cd .. 
+cd ..
+cd PiHole
+bash pirunner.sh
+cd ..
+bash filerserver.sh
+bash glancesrunner.sh
+bash plexrunner.sh
 
 
 #Starts all Containers
