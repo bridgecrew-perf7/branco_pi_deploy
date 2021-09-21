992b00a1fc2780e112d34fd389cb72a9e9c60d60
chucky747 <90802927+chucky747@users.noreply.github.com>
2021-09-21 14:01:03 -0400

    Update update_containers.sh

--- update_containers.sh
+++ update_containers.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Kills all Containers
+echo /////////////////////
+echo Stoping Containers...
+echi /////////////////////
 docker kill $(docker ps -q)
    
 # Pulls images and Runs Containers and updates them
+echo /////////////////////
+echo Pulling Images...
+echi /////////////////////
 bash portainer.sh
 cd NGINX
 cd Proxy
@@ -23,8 +29,16 @@ bash fileserver.sh
 bash glancesrunner.sh
 bash plexrunner.sh
 
+echo /////////////////////
+echo Images Updated... Starting Containers.
+echi /////////////////////
+
 #Starts all Containers
 docker start $(docker ps -a -q)
+
+echo /////////////////////
+echo Local IPs Bellow:
+echi /////////////////////
 
 echo Plex: @localip:32400
 echo Protainer: @localip:8090
