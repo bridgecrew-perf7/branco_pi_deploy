74c61b60a83c13d0523620b4edc6aa9355a2fe43
chucky747 <90802927+chucky747@users.noreply.github.com>
2021-09-21 16:42:01 -0400

    Add files via upload

---
+++ Pi Admin/UpdatePI.sh
@@ -0,0 +1,12 @@
+echo
+echo
+echo -----------------------------------------
+echo
+echo SShing into pi...
+echo
+echo You will be prompted twice for sudo. Please input sudo password and confirm.
+echo
+echo --------------------------------------------
+echo
+echo
+ssh -t pi@192.168.0.221 'cd /; cd /media/pi/pi-ssd/pi-setup/branco_pi_deploy; git pull; sudo bash update_containers.sh'
