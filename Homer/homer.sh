c97ca4ff3b8a95bd64601e27478d84d9cabb85f3
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-11 12:49:10 -0500

    Update homer.sh

--- Homer/homer.sh
+++ Homer/homer.sh
@@ -6,14 +6,14 @@ docker rm -f homer
 docker run -d \
   --name homer \
   -p 8089:8080 \
-  -v /home/homer/assets/:/www/assets \
+  -v /home/pi/homer/assets/:/www/assets \
   --restart=always \
   b4bz/homer:latest
 
 
-cp /home/james/raspberrypi-dev-setup/Homer/config.yml /home/homer/assets/config.yml
-sudo rm -rfv /home/homer/assets/branconet_assets/
-sudo cp -r /home/james/raspberrypi-dev-setup/Homer/branconet_assets /home/homer/assets/branconet_assets/
+cp /home/pi/raspberrypi-dev-setup/Homer/config.yml /home/homer/assets/config.yml
+sudo rm -rfv /home/pi/homer/assets/branconet_assets/
+sudo cp -r /home/pi/raspberrypi-dev-setup/Homer/branconet_assets /home/homer/assets/branconet_assets/
 
 
   echo Finished $ip:8081 or homer.box:8091
