b31588c002adbb7a6d442baa71f11fd1f382401f
jtmb <86915618+jtmb@users.noreply.github.com>
2022-01-28 16:11:40 -0500

    Update ghost.sh

--- ghost.sh
+++ ghost.sh
@@ -1,7 +1,7 @@
 sudo docker rm -f ghost
 
 
-sudo docker run -d --name ghost -e url=https://ana.branconet.com -p 8088:2368 -v /home/pi/container-program-files/ghost-blog:/var/lib/ghost/content ghost:alpine
+sudo docker run -d --name ghost -e url=https://anita.branconet.com -p 8088:2368 -v /home/pi/container-program-files/ghost-blog:/var/lib/ghost/content ghost:alpine
 
 sudo docker exec -it ghost apk update && apk add fail2ban -y
 sudo docker update --restart unless-stopped ghost
